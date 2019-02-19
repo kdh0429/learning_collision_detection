16a6626ae3b86323bc95a00d119135313e2315db
kdh0429 <kdh0429@snu.ac.kr>
2019-02-19 21:02:53 +0900

    Run first experimental learning

---
+++ collision_detection_7dof_txt_to_csv.m
@@ -0,0 +1,11 @@
+RawData = load('training_data.txt');
+
+Training = RawData(randperm(fix(size(RawData,1)*1.0)),:);
+Validation = RawData(randperm(fix(size(RawData,1)*1.0)),:);
+Testing = RawData(randperm(fix(size(RawData,1)*1.0)),:);
+
+RawData(:,86) = -RawData(:,85)+1;
+csvwrite('raw_data_.csv', RawData);
+%csvwrite('training_data_.csv', Training);
+%csvwrite('validation_data_.csv', Validation);
+%csvwrite('testing_data_.csv', Testing);
---
+++ collision_detection_result.py
@@ -0,0 +1,65 @@
+import tensorflow as tf
+import numpy as np
+import csv
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
+
+# parameters
+num_input = 42
+num_output = 2
+
+
+path = 'raw_data_.csv'
+# raw data
+f = open(path, 'r', encoding='utf-8')
+rdr = csv.reader(f)
+t = []
+x_data_raw = []
+y_data_raw = []
+
+for line in rdr:
+    line = [float(i) for i in line]
+    t.append(line[0])
+    x_data_raw.append(line[1:num_input+1])
+    y_data_raw.append(line[-num_output:])
+
+t = np.reshape(t,(-1,1))
+x_data_raw = np.reshape(x_data_raw, (-1, num_input))
+y_data_raw = np.reshape(y_data_raw, (-1, num_output))
+
+tf.reset_default_graph()
+sess = tf.Session()
+
+new_saver = tf.train.import_meta_graph('model/model.ckpt.meta')
+new_saver.restore(sess, 'model/model.ckpt')
+
+graph = tf.get_default_graph()
+x = graph.get_tensor_by_name("m1/input:0")
+y = graph.get_tensor_by_name("m1/output:0")
+keep_prob = graph.get_tensor_by_name("m1/keep_prob:0")
+hypothesis = graph.get_tensor_by_name("m1/hypothesis:0")
+
+hypo = sess.run(hypothesis, feed_dict={x: x_data_raw, keep_prob: 1.0})
+
+# mean_error = graph.get_tensor_by_name("mean_error:0")
+# for op in graph.get_operations():
+#     print(op.name)
+
+# mean_error_test = sess.run(mean_error, feed_dict={x: x_data_raw, y: y_data_raw, keep_prob: 1.0})
+# print(mean_error_test)
+# print(np.mean(np.abs(y_data_raw - hypo)))
+
+
+prediction = np.argmax(hypo, 1)
+correct_prediction = np.equal(prediction, np.argmax(y_data_raw, 1))
+accuracy = np.mean(correct_prediction)
+
+print("Accuracy : %f" % accuracy)
+
+
+plt.plot(t,y_data_raw[:,0], 'r', label='real')
+plt.plot(t,hypo[:,0], 'b', label='prediction')
+plt.xlabel('time')
+plt.ylabel('Collision Probability')
+plt.legend()
+plt.show()
