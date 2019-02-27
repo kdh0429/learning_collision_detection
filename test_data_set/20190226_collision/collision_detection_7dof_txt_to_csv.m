94b8b6476172730b66f129d6c4bb59539af46c37
Donghyeon Kim <kdh0429@snu.ac.kr>
2019-02-27 09:22:58 +0900

    Create 300 test set

---
+++ test_data_set/20190226_collision/collision_detection_7dof_txt_to_csv.m
@@ -0,0 +1,13 @@
+RawData = load('training_data.txt');
+
+%RawData(:,86) = -RawData(:,85)+1;
+
+Training = RawData(randperm(fix(size(RawData,1)*1.0)),:);
+Validation = RawData(randperm(fix(size(RawData,1)*1.0)),:);
+Testing = RawData(randperm(fix(size(RawData,1)*1.0)),:);
+
+
+csvwrite('raw_data_.csv', RawData);
+%csvwrite('training_data_.csv', Training);
+%csvwrite('validation_data_.csv', Validation);
+csvwrite('testing_data_.csv', Testing);
