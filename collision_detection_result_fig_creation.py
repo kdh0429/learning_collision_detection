ef33b71327c837d14dcc9d2aa9741bec60aeb37e
kdh0429 <kdh0429@snu.ac.kr>
2019-03-04 12:29:32 +0900

    Edit original code to dynamic/control detached version

--- collision_detection_result_fig_creation.py
+++ collision_detection_result_fig_creation.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 from sklearn import preprocessing
 
 # parameters
-num_input = 42
+num_input_dyna = 28
+num_input_cont = 14
 num_output = 2
 
 
@@ -15,18 +16,19 @@ for i in range(10):
     f = open(path, 'r', encoding='utf-8')
     rdr = csv.reader(f)
     t = []
-    x_data_raw = []
+    x_dyna_data_raw = []
+    x_cont_data_raw = []
     y_data_raw = []
 
     for line in rdr:
         line = [float(i) for i in line]
-        #t.append(line[0])
-        x_data_raw.append(line[1:num_input+1])
-        #x_data_raw.append(line[29:43])
+        x_dyna_data_raw.append(line[1:num_input_dyna+1])
+        x_cont_data_raw.append(line[num_input_dyna+1:num_input_dyna+num_input_cont+1])
         y_data_raw.append(line[-num_output:])
-    t = range(len(x_data_raw))
+    t = range(len(x_dyna_data_raw)+len(x_cont_data_raw))
     t = np.reshape(t,(-1,1))
-    x_data_raw = np.reshape(x_data_raw, (-1, num_input))
+    x_dyna_data_raw = np.reshape(x_dyna_data_raw, (-1, num_input_dyna))
+    x_cont_data_raw = np.reshape(x_cont_data_raw, (-1, num_input_cont))
     y_data_raw = np.reshape(y_data_raw, (-1, num_output))
 
     tf.reset_default_graph()
@@ -36,12 +38,13 @@ for i in range(10):
     new_saver.restore(sess, 'model/model.ckpt')
 
     graph = tf.get_default_graph()
-    x = graph.get_tensor_by_name("m1/input:0")
+    x_dyna = graph.get_tensor_by_name("m1/input_dyna:0")
+    x_cont = graph.get_tensor_by_name("m1/input_cont:0")
     y = graph.get_tensor_by_name("m1/output:0")
     keep_prob = graph.get_tensor_by_name("m1/keep_prob:0")
     hypothesis = graph.get_tensor_by_name("m1/hypothesis:0")
 
-    hypo = sess.run(hypothesis, feed_dict={x: x_data_raw, keep_prob: 1.0})
+    hypo = sess.run(hypothesis, feed_dict={x_dyna: x_dyna_data_raw, x_cont : x_cont_data_raw, keep_prob: 1.0})
 
     prediction = np.argmax(hypo, 1)
     correct_prediction = np.equal(prediction, np.argmax(y_data_raw, 1))
@@ -57,4 +60,3 @@ for i in range(10):
     plt.legend()
     plt.savefig('Figure_' + str(i)+'.png')
     plt.clf()
-    #plt.show()
