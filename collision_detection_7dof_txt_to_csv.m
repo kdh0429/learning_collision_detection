bf0cc5f1948c02182660a5198947f138b1215805
kdh0429 <kdh0429@snu.ac.kr>
2019-03-07 17:36:26 +0900

    Edit qddot in .txt

--- collision_detection_7dof_txt_to_csv.m
+++ collision_detection_7dof_txt_to_csv.m
@@ -1,17 +1,8 @@
-RawData = load('training_data_20190226_collision.txt');
-for i=2:size(RawData,1)
-    RawData(i,23) = (RawData(i,16)-RawData(i-1,16))*100;
-    RawData(i,24) = (RawData(i,17)-RawData(i-1,17))*100;
-    RawData(i,25) = (RawData(i,18)-RawData(i-1,18))*100;
-    RawData(i,26) = (RawData(i,19)-RawData(i-1,19))*100;
-    RawData(i,27) = (RawData(i,20)-RawData(i-1,20))*100;
-    RawData(i,28) = (RawData(i,21)-RawData(i-1,21))*100;
-    RawData(i,29) = (RawData(i,22)-RawData(i-1,22))*100;
-end
+RawData = load('training_data.txt');
+
 Training = RawData(randperm(fix(size(RawData,1)*1.0)),:);
 Validation = RawData(randperm(fix(size(RawData,1)*1.0)),:);
 Testing = RawData(randperm(fix(size(RawData,1)*1.0)),:);
-
 
 csvwrite('raw_data_.csv', RawData);
 csvwrite('training_data_.csv', Training);
