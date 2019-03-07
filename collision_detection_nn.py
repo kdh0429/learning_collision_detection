5997ad3c32bdd9430be8e95b102933c343393d24
kdh0429 <kdh0429@snu.ac.kr>
2019-03-07 17:12:02 +0900

    1

--- collision_detection_nn.py
+++ collision_detection_nn.py
@@ -10,7 +10,7 @@ import os
 wandb_use = True
 start_time = time.time()
 if wandb_use == True:
-    wandb.init(project="collision_detection_dyna_cont_detached", tensorboard=False)
+    wandb.init(project="collision_detection_residual", tensorboard=False)
 
 class Model:
 
@@ -21,157 +21,147 @@ class Model:
 
     def _build_net(self):
         with tf.variable_scope(self.name):
-            self.X_dyna = tf.placeholder(tf.float32, shape=[None, num_input_dyna], name = "input_dyna")
-            self.X_cont = tf.placeholder(tf.float32, shape=[None, num_input_cont], name = "input_cont")
-            self.Y = tf.placeholder(tf.int64, shape=[None, num_output], name= "output")
+            self.X = tf.placeholder(tf.float32, shape=[None, num_input], name = "input")
+            self.Y = tf.placeholder(tf.float32, shape=[None, num_output], name= "output")
             self.keep_prob = tf.placeholder(tf.float32, name="keep_prob")
-
-            self.hidden_layers_dyna = 0
-            self.hidden_layers_cont = 0
+            self.hidden_layers = 0
             self.hidden_neurons = 40
 
             # weights & bias for nn layers
             # http://stackoverflow.com/questions/33640581/how-to-do-xavier-initialization-on-tensorflow
-            W1_dyna = tf.get_variable("W1_dyna", shape=[num_input_dyna, self.hidden_neurons], initializer=tf.contrib.layers.xavier_initializer())
-            b1_dyna = tf.Variable(tf.random_normal([self.hidden_neurons]))
-            L1_dyna = tf.matmul(self.X_dyna, W1_dyna) + b1_dyna
-            L1_dyna = tf.nn.sigmoid(L1_dyna)
-            L1_dyna = tf.nn.dropout(L1_dyna, keep_prob=self.keep_prob)
+            W1 = tf.get_variable("W1", shape=[num_input, self.hidden_neurons], initializer=tf.contrib.layers.xavier_initializer())
+            b1 = tf.Variable(tf.random_normal([self.hidden_neurons]))
+            L1 = tf.matmul(self.X, W1) +b1
+            L1 = tf.nn.relu(L1)
+            L1 = tf.nn.dropout(L1, keep_prob=self.keep_prob)
 
-            W2_dyna = tf.get_variable("W2_dyna", shape=[self.hidden_neurons, self.hidden_neurons], initializer=tf.contrib.layers.xavier_initializer())
-            b2_dyna = tf.Variable(tf.random_normal([self.hidden_neurons]))
-            L2_dyna = tf.matmul(L1_dyna, W2_dyna) + b2_dyna
-            L2_dyna = tf.nn.sigmoid(L2_dyna)
-            L2_dyna = tf.nn.dropout(L2_dyna, keep_prob=self.keep_prob)
-            self.hidden_layers_dyna += 1
+            W2 = tf.get_variable("W2", shape=[self.hidden_neurons, self.hidden_neurons], initializer=tf.contrib.layers.xavier_initializer())
+            b2 = tf.Variable(tf.random_normal([self.hidden_neurons]))
+            L2 = tf.matmul(L1, W2) +b2
+            L2 = tf.nn.relu(L2)
+            L2 = tf.nn.dropout(L2, keep_prob=self.keep_prob)
+            self.hidden_layers += 1
 
-            W3_dyna = tf.get_variable("W3_dyna", shape=[self.hidden_neurons, self.hidden_neurons], initializer=tf.contrib.layers.xavier_initializer())
-            b3_dyna = tf.Variable(tf.random_normal([self.hidden_neurons]))
-            L3_dyna = tf.matmul(L2_dyna, W3_dyna) + b3_dyna
-            L3_dyna = tf.nn.sigmoid(L3_dyna)
-            L3_dyna = tf.nn.dropout(L3_dyna, keep_prob=self.keep_prob)
-            self.hidden_layers_dyna += 1
+            W3 = tf.get_variable("W3", shape=[self.hidden_neurons, self.hidden_neurons], initializer=tf.contrib.layers.xavier_initializer())
+            b3 = tf.Variable(tf.random_normal([self.hidden_neurons]))
+            L3 = tf.matmul(L2, W3) +b3
+            L3 = tf.nn.relu(L3)
+            L3 = tf.nn.dropout(L3, keep_prob=self.keep_prob)
+            self.hidden_layers += 1
 
-            W4_dyna = tf.get_variable("W4_dyna", shape=[self.hidden_neurons, self.hidden_neurons], initializer=tf.contrib.layers.xavier_initializer())
-            b4_dyna = tf.Variable(tf.random_normal([self.hidden_neurons]))
-            L4_dyna = tf.matmul(L3_dyna, W4_dyna) +b4_dyna
-            L4_dyna = tf.nn.sigmoid(L4_dyna)
-            L4_dyna = tf.nn.dropout(L4_dyna, keep_prob=self.keep_prob)
-            self.hidden_layers_dyna += 1
+            W4 = tf.get_variable("W4", shape=[self.hidden_neurons, self.hidden_neurons], initializer=tf.contrib.layers.xavier_initializer())
+            b4 = tf.Variable(tf.random_normal([self.hidden_neurons]))
+            L4 = tf.matmul(L3, W4) +b4
+            L4 = tf.nn.relu(L4)
+            L4 = tf.nn.dropout(L4, keep_prob=self.keep_prob)
+            self.hidden_layers += 1
 
-            W5_dyna = tf.get_variable("W5_dyna", shape=[self.hidden_neurons, 1], initializer=tf.contrib.layers.xavier_initializer())
-            b5_dyna = tf.Variable(tf.random_normal([self.hidden_neurons]))
-            L5_dyna = tf.matmul(L4_dyna, W5_dyna) +b5_dyna
-            L5_dyna = tf.nn.sigmoid(L5_dyna)
-            L5_dyna = tf.nn.dropout(L5_dyna, keep_prob=self.keep_prob)
-            self.hidden_layers_dyna += 1
+            #W5 = tf.get_variable("W5", shape=[self.hidden_neurons, self.hidden_neurons], initializer=tf.contrib.layers.xavier_initializer())
+            #b5 = tf.Variable(tf.random_normal([self.hidden_neurons]))
+            #L5 = tf.matmul(L4, W5) +b5
+            #L5 = tf.nn.sigmoid(L5)
+            #L5 = tf.nn.dropout(L5, keep_prob=self.keep_prob)
+            #self.hidden_layers += 1
 
-            W1_cont = tf.get_variable("W1_cont", shape=[num_input_cont, self.hidden_neurons], initializer=tf.contrib.layers.xavier_initializer())
-            b1_cont = tf.Variable(tf.random_normal([self.hidden_neurons]))
-            L1_cont = tf.matmul(self.X_cont, W1_cont) +b1_cont
-            L1_cont = tf.nn.sigmoid(L1_cont)
-            L1_cont = tf.nn.dropout(L1_cont, keep_prob=self.keep_prob)
+            # W6 = tf.get_variable("W6", shape=[hidden_neurons, hidden_neurons], initializer=tf.contrib.layers.xavier_initializer())
+            # b6 = tf.Variable(tf.random_normal([hidden_neurons]))
+            # L6 = tf.matmul(L5, W6) +b6
+            # L6 = tf.nn.sigmoid(L6)
+            # L6 = tf.nn.dropout(L6, keep_prob=self.keep_prob)
+            # self.hidden_layers += 1
 
-            W2_cont = tf.get_variable("W2_cont", shape=[self.hidden_neurons, self.hidden_neurons], initializer=tf.contrib.layers.xavier_initializer())
-            b2_cont = tf.Variable(tf.random_normal([self.hidden_neurons]))
-            L2_cont = tf.matmul(L1_cont, W2_cont) +b2_cont
-            L2_cont = tf.nn.sigmoid(L2_cont)
-            L2_cont = tf.nn.dropout(L2_cont, keep_prob=self.keep_prob)
-            self.hidden_layers_cont += 1
+            # W7 = tf.get_variable("W7", shape=[hidden_neurons, hidden_neurons], initializer=tf.contrib.layers.xavier_initializer())
+            # b7 = tf.Variable(tf.random_normal([hidden_neurons]))
+            # L7 = tf.matmul(L6, W7) +b7
+            # L7 = tf.nn.sigmoid(L7)
+            # L7 = tf.nn.dropout(L7, keep_prob=self.keep_prob)
+            # self.hidden_layers += 1
 
-            W3_cont = tf.get_variable("W3_cont", shape=[self.hidden_neurons, 1], initializer=tf.contrib.layers.xavier_initializer())
-            b3_cont = tf.Variable(tf.random_normal([self.hidden_neurons]))
-            L3_cont = tf.matmul(L2_cont, W3_cont) +b3_cont
-            L3_cont = tf.nn.sigmoid(L3_cont)
-            L3_cont = tf.nn.dropout(L3_cont, keep_prob=self.keep_prob)
-            self.hidden_layers_cont += 1
+            # W8 = tf.get_variable("W8", shape=[hidden_neurons, hidden_neurons], initializer=tf.contrib.layers.xavier_initializer())
+            # b8 = tf.Variable(tf.random_normal([hidden_neurons]))
+            # L8 = tf.matmul(L7, W8) +b8
+            # #L8 = tf.nn.relu(L8)
+            # L8 = tf.nn.sigmoid(L8)
+            # L8 = tf.nn.dropout(L8, keep_prob=self.keep_prob)
+            # self.hidden_layers += 1
 
-            L_last = tf.stack([L5_dyna, L3_cont])
-            W_last = tf.get_variable("W_last", shape=[2, num_output], initializer=tf.contrib.layers.xavier_initializer())
-            b_last = tf.Variable(tf.random_normal([num_output]))
-            self.logits = tf.matmul(L_last, W_last) + b_last
-            self.hypothesis = tf.nn.softmax(self.logits)
+            W9 = tf.get_variable("W9", shape=[self.hidden_neurons, num_output], initializer=tf.contrib.layers.xavier_initializer())
+            b9 = tf.Variable(tf.random_normal([num_output]))
+            self.hypothesis = tf.matmul(L4,W9)+b9
             self.hypothesis = tf.identity(self.hypothesis, "hypothesis")
 
             # define cost/loss & optimizer
-            self.l2_reg = 0 #tf.nn.l2_loss(W1) + tf.nn.l2_loss(W2) + tf.nn.l2_loss(W3) + tf.nn.l2_loss(W4) + tf.nn.l2_loss(W5) + tf.nn.l2_loss(W6) + tf.nn.l2_loss(W7)
+            self.l2_reg = tf.nn.l2_loss(W1)# + tf.nn.l2_loss(W2) + tf.nn.l2_loss(W3) + tf.nn.l2_loss(W4) + tf.nn.l2_loss(W5) + tf.nn.l2_loss(W6) + tf.nn.l2_loss(W7)
             self.l2_reg = regul_factor* self.l2_reg
-            self.cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.logits, labels=self.Y))
-            #self.cost = tf.reduce_mean(tf.reduce_mean(tf.square(self.hypothesis - self.Y)))
+            self.cost = tf.reduce_mean(tf.reduce_mean(tf.square(self.hypothesis - self.Y)))
             self.optimizer = tf.train.AdamOptimizer(learning_rate= learning_rate).minimize(self.cost + self.l2_reg)
         
-        self.prediction = tf.argmax(self.hypothesis, 1)
-        self.correct_prediction = tf.equal(self.prediction, tf.argmax(self.Y, 1))
-        self.accuracy = tf.reduce_mean(tf.cast(self.correct_prediction, tf.float32))
+        self.mean_error = tf.reduce_mean(tf.abs(self.Y-self.hypothesis))
+        self.mean_error = tf.identity(self.mean_error, "mean_error")
 
-    def get_mean_error_hypothesis(self, x_dyna_test, x_cont_test, y_test, keep_prop=1.0):
-        return self.sess.run([self.accuracy, self.hypothesis, self.X_dyna, self.X_cont, self.Y, self.l2_reg], feed_dict={self.X_dyna: x_dyna_test, self.X_cont: x_cont_test, self.Y: y_test, self.keep_prob: keep_prop})
+    def get_mean_error_hypothesis(self, x_test, y_test, keep_prop=1.0):
+        return self.sess.run([self.mean_error, self.hypothesis, self.X, self.Y, self.l2_reg], feed_dict={self.X: x_test, self.Y: y_test, self.keep_prob: keep_prop})
 
-    def train(self, x_dyna_data, x_cont_data, y_data, keep_prop=1.0):
-        return self.sess.run([self.accuracy, self.l2_reg, self.optimizer], feed_dict={
-            self.X_dyna: x_dyna_data, self.X_cont: x_cont_data, self.Y: y_data, self.keep_prob: keep_prop})
+    def train(self, x_data, y_data, keep_prop=1.0):
+        return self.sess.run([self.mean_error, self.l2_reg, self.optimizer], feed_dict={
+            self.X: x_data, self.Y: y_data, self.keep_prob: keep_prop})
 
     def next_batch(self, num, data):
-        x_dyna_batch = []
-        x_cont_batch = []
+        x_batch = []
         y_batch = []
         i = 0
         for line in data:
             line = [float(i) for i in line]
-            x_dyna_batch.append(line[1:num_input_dyna+1])
-            x_cont_batch.append(line[num_input_dyna+1:num_input_dyna+num_input_cont+1])
-            y_batch.append(line[-num_output:])
+            if(line[84] == 0):
+                x_batch.append(line[1:num_input+1])
+                y_batch.append(line[num_input+1+7:num_input+1+7+num_output])
             i = i+1
 
             if i == num:
                 break
-        return [np.asarray(np.reshape(x_dyna_batch, (-1, x_dyna_batch))), np.asarray(np.reshape(x_cont_batch, (-1, num_input_cont))), np.asarray(np.reshape(y_batch,(-1,num_output)))]
+        return [np.asarray(np.reshape(x_batch, (-1, num_input))), np.asarray(np.reshape(y_batch,(-1,num_output))), ]
     def get_hidden_number(self):
-        return [self.hidden_layers_dyna, self.hidden_layers_cont, self.hidden_neurons]
+        return [self.hidden_layers, self.hidden_neurons]
 
 # input/output number
-num_input_dyna = 28
-num_input_cont = 14
-num_output = 2
+num_input = 21
+num_output = 7
 
 # parameters
-learning_rate = 0.000010 #0.000001
-training_epochs = 3000
-batch_size = 100
-total_batch = 1800
+learning_rate = 0.00010 #0.000001
+training_epochs = 1000
+batch_size = 150
+total_batch = 1200
 drop_out = 1.0
 regul_factor = 0.00000
 
 # loading testing data
 f_test = open('testing_data_.csv', 'r', encoding='utf-8')
 rdr_test = csv.reader(f_test)
-x_dyna_data_test = []
-x_cont_data_test = []
+x_data_test = []
 y_data_test = []
 
 for line in rdr_test:
     line = [float(i) for i in line]
-    x_dyna_data_test.append(line[1:num_input_dyna+1])
-    x_cont_data_test.append(line[num_input_dyna+1:num_input_dyna+num_input_cont+1])
-    y_data_test.append(line[-num_output:])
+    if line[84]==0 :
+        x_data_test.append(line[1:num_input+1])
+        y_data_test.append(line[num_input+1+7:num_input+1+7+num_output])
 
-x_dyna_data_test = np.reshape(x_dyna_data_test, (-1, num_input_dyna))
-x_cont_data_test = np.reshape(x_cont_data_test, (-1, num_input_cont))
+x_data_test = np.reshape(x_data_test, (-1, num_input))
 y_data_test = np.reshape(y_data_test, (-1, num_output))
 
 # load validation data
 f_val = open('validation_data_.csv', 'r', encoding='utf-8')
 rdr_val = csv.reader(f_val)
-x_dyna_data_val = []
-x_cont_data_val = []
+x_data_val = []
 y_data_val = []
 for line in rdr_val:
     line = [float(i) for i in line]
-    x_dyna_data_val.append(line[1:num_input_dyna+1])
-    x_cont_data_val.append(line[num_input_dyna+1:num_input_dyna+num_input_cont+1])
-    y_data_val.append(line[-num_output:])
-x_dyna_data_val = np.reshape(x_dyna_data_val, (-1, num_input_dyna))
-x_cont_data_val = np.reshape(x_cont_data_val, (-1, num_input_cont))
+    if line[84]==0 :
+        x_data_val.append(line[1:num_input+1])
+        y_data_val.append(line[num_input+1+7:num_input+1+7+num_output])
+
+x_data_val = np.reshape(x_data_val, (-1, num_input))
 y_data_val = np.reshape(y_data_val, (-1, num_output))
 
 
@@ -186,13 +176,12 @@ if wandb_use == True:
     wandb.config.batch_size = batch_size
     wandb.config.learning_rate = learning_rate
     wandb.config.drop_out = drop_out
-    wandb.config.num_input_dyna = num_input_dyna
-    wandb.config.num_input_dyna = num_input_cont
+    wandb.config.num_input = num_input
     wandb.config.num_output = num_output
     wandb.config.total_batch = total_batch
-    wandb.config.activation_function = "Sigmoid"
+    wandb.config.activation_function = "ReLU"
     wandb.config.training_episode = 1200
-    wandb.config.hidden_layers_dyna, wandb.config.hidden_layers_cont, wandb.config.hidden_neurons = m1.get_hidden_number()
+    wandb.config.hidden_layers, wandb.config.hidden_neurons = m1.get_hidden_number()
     wandb.config.L2_regularization = regul_factor 
 
 # train my model
@@ -206,16 +195,16 @@ for epoch in range(training_epochs):
     rdr = csv.reader(f)
 
     for i in range(total_batch):
-        batch_xs_dyna, batch_xs_cont, batch_ys = m1.next_batch(batch_size, rdr)
-        c, reg_c,_ = m1.train(batch_xs_dyna, batch_xs_cont, batch_ys, drop_out)
+        batch_xs, batch_ys = m1.next_batch(batch_size, rdr)
+        c, reg_c,_ = m1.train(batch_xs, batch_ys, drop_out)
         accu_train += c / total_batch
         avg_reg_cost += reg_c / total_batch
 
     print('Epoch:', '%04d' % (epoch + 1))
-    print('Train Accuracy =', '{:.9f}'.format(accu_train), 'Train l2 reg cost =', '{:.9f}'.format(avg_reg_cost))
+    print('Train error =', '{:.9f}'.format(accu_train), 'Train l2 reg cost =', '{:.9f}'.format(avg_reg_cost))
 
-    [accu_val, hypo, x_dyna_val, x_cont_val, y_val, l2_reg_val] = m1.get_mean_error_hypothesis(x_dyna_data_val,x_cont_data_val, y_data_val)
-    print('Validation Accuracy:', '{:.9f}'.format(accu_val), 'Validation l2 regularization:', '{:.9f}'.format(l2_reg_val))
+    [accu_val, hypo, x_val, y_val, l2_reg_val] = m1.get_mean_error_hypothesis(x_data_val, y_data_val)
+    print('Validation error:', '{:.9f}'.format(accu_val), 'Validation l2 regularization:', '{:.9f}'.format(l2_reg_val))
 
     train_mse[epoch] = accu_train
     validation_mse[epoch] = accu_val
@@ -223,16 +212,16 @@ for epoch in range(training_epochs):
     if wandb_use == True:
         wandb.log({'training Accuracy': accu_train, 'validation Accuracy': accu_val, 'validation l2_reg': l2_reg_val})
 
-        if epoch % 20 ==0:
+        if epoch % 20 == 0:
             for var in tf.trainable_variables():
                 name = var.name
                 wandb.log({name: sess.run(var)})
 
 
 print('Learning Finished!')
-[accu_test, hypo, x_dyna_test, x_cont_test, y_test, l2_reg_test] = m1.get_mean_error_hypothesis(x_dyna_data_test, x_cont_data_test, y_data_test)
+[accu_test, hypo, x_test, y_test, l2_reg_test] = m1.get_mean_error_hypothesis(x_data_test, y_data_test)
 # print('Error: ', error,"\n x_data: ", x_test,"\nHypothesis: ", hypo, "\n y_data: ", y_test)
-print('Test Accuracy: ', accu_test)
+print('Test Error: ', accu_test)
 print('Test l2 regularization:', l2_reg_test)
 
 elapsed_time = time.time() - start_time
--- collision_detection_result_fig_creation.py
+++ collision_detection_result_fig_creation.py
@@ -5,9 +5,8 @@ import matplotlib.pyplot as plt
 from sklearn import preprocessing
 
 # parameters
-num_input_dyna = 28
-num_input_cont = 14
-num_output = 2
+num_input = 21
+num_output = 7
 
 
 for i in range(10):
@@ -16,19 +15,17 @@ for i in range(10):
     f = open(path, 'r', encoding='utf-8')
     rdr = csv.reader(f)
     t = []
-    x_dyna_data_raw = []
-    x_cont_data_raw = []
+    x_data_raw = []
     y_data_raw = []
 
     for line in rdr:
         line = [float(i) for i in line]
-        x_dyna_data_raw.append(line[1:num_input_dyna+1])
-        x_cont_data_raw.append(line[num_input_dyna+1:num_input_dyna+num_input_cont+1])
-        y_data_raw.append(line[-num_output:])
-    t = range(len(x_dyna_data_raw)+len(x_cont_data_raw))
+        #if(line[84] == 0):
+        x_data_raw.append(line[1:num_input+1])
+        y_data_raw.append(line[num_input+1+7:num_input+1+7+num_output])
+    t = range(len(x_data_raw))
     t = np.reshape(t,(-1,1))
-    x_dyna_data_raw = np.reshape(x_dyna_data_raw, (-1, num_input_dyna))
-    x_cont_data_raw = np.reshape(x_cont_data_raw, (-1, num_input_cont))
+    x_data_raw = np.reshape(x_data_raw, (-1, num_input))
     y_data_raw = np.reshape(y_data_raw, (-1, num_output))
 
     tf.reset_default_graph()
@@ -38,25 +35,30 @@ for i in range(10):
     new_saver.restore(sess, 'model/model.ckpt')
 
     graph = tf.get_default_graph()
-    x_dyna = graph.get_tensor_by_name("m1/input_dyna:0")
-    x_cont = graph.get_tensor_by_name("m1/input_cont:0")
+    x = graph.get_tensor_by_name("m1/input:0")
     y = graph.get_tensor_by_name("m1/output:0")
     keep_prob = graph.get_tensor_by_name("m1/keep_prob:0")
     hypothesis = graph.get_tensor_by_name("m1/hypothesis:0")
 
-    hypo = sess.run(hypothesis, feed_dict={x_dyna: x_dyna_data_raw, x_cont : x_cont_data_raw, keep_prob: 1.0})
+    hypo = sess.run(hypothesis, feed_dict={x: x_data_raw, keep_prob: 1.0})
 
-    prediction = np.argmax(hypo, 1)
-    correct_prediction = np.equal(prediction, np.argmax(y_data_raw, 1))
-    accuracy = np.mean(correct_prediction)
+    mean_error_1 = np.mean(np.abs(y_data_raw[:,0]-hypo[:,0]))
+    mean_error_2 = np.mean(np.abs(y_data_raw[:,1]-hypo[:,1]))
+    mean_error_3 = np.mean(np.abs(y_data_raw[:,2]-hypo[:,2]))
+    mean_error_4 = np.mean(np.abs(y_data_raw[:,3]-hypo[:,3]))
+    mean_error_5 = np.mean(np.abs(y_data_raw[:,4]-hypo[:,4]))
+    mean_error_6 = np.mean(np.abs(y_data_raw[:,5]-hypo[:,5]))
+    mean_error_7 = np.mean(np.abs(y_data_raw[:,6]-hypo[:,6]))
+    print("Accuracy : %f" % mean_error_1)
 
-    print("Accuracy : %f" % accuracy)
+    for j in range(7):
+        plt.subplot(7,1,j+1)
+        plt.plot(t,y_data_raw[:,j], color='r', label='real')
+        plt.plot(t,hypo[:,0], color='b', label='prediction')
+        plt.xlabel('time')
+        plt.ylabel('qdot')
+        plt.legend()
 
-
-    plt.plot(t,y_data_raw[:,0], color='r', marker="o", label='real')
-    plt.plot(t,hypo[:,0], color='b',marker="x", label='prediction')
-    plt.xlabel('time')
-    plt.ylabel('Collision Probability')
-    plt.legend()
     plt.savefig('Figure_' + str(i)+'.png')
     plt.clf()
+    #plt.show()
